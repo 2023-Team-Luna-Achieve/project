788177379a1a94ca065b107aad33cf5b8fc5f5e4
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-10-10 15:18:17 +0900

    Feat:메인 페이지 퍼블리싱

--- frontend/src/pages/BannerSlider.tsx
+++ frontend/src/pages/BannerSlider.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 
 const BannerSlider: React.FC = () => {
   const settings = {
-    className: 'center',
     centerMode: true,
     centerPadding: '60px',
     dots: true,
@@ -18,35 +17,150 @@ const BannerSlider: React.FC = () => {
 
   const Banner = styled.div`
     width: 100%;
-    height: 40%;
+    height: 100%;
     overflow: visible;
+    position: relative;
   `;
 
   const Background = styled.div`
-    background-color: #ffeffa;
+    background-color: #ececec;
     width: 100%;
     height: 100%;
-    margin-left: 100px;
-    margin-right: 100px;
+    color: white;
+    border-radius: 10px;
+    padding: 20px;
+    position: relative;
+  `;
+
+  const Heading = styled.h1`
+    margin: 0;
+    margin-top: 60px;
+    margin-bottom: 5px;
+    margin-left: 34px;
+  `;
+
+  const Heading2 = styled.h1`
+    margin: 0;
+    margin-top: 0px;
+    margin-bottom: 5px;
+    margin-left: 34px;
+  `;
+
+  const Text = styled.p`
+    margin-bottom: 30px;
+    margin-left: 34px;
+  `;
+
+  const StudyLabel = styled.span`
+    position: absolute;
+    top: 34px;
+    left: 16px;
+    background-color: black;
+    color: white;
+    padding: 4px 8px;
+    border-radius: 5px;
+    margin-left: 34px;
   `;
 
   const StyledSlider = styled(Slider)`
     height: 100%;
+    .slick-list {
+      height: 100%;
+    }
+    .slick-slide {
+      margin-left: 40px;
+      margin-right: 20px;
+      height: 100%;
+    }
+  `;
+
+  const RoundedRectangle = styled.div`
+    position: absolute;
+    width: 700px;
+    height: 194px;
+    left: 20px;
+    top: 600px;
+    background: #ffffff;
+    box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.15);
+    border-radius: 40px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    h3 {
+      font-weight: 300;
+      text-align: left;
+      line-height: 1.2; /* 줄 간격을 조절합니다. */
+    }
+
+    h2 {
+      font-weight: 600;
+      text-align: left;
+      line-height: 1.2; /* 줄 간격을 조절합니다. */
+    }
+  `;
+
+  const RoundedRectangle2 = styled.div`
+    position: absolute;
+    width: 700px;
+    height: 194px;
+    left: 740px;
+    top: 600px;
+    background: #ffffff;
+    box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.15);
+    border-radius: 40px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    h3 {
+      font-weight: 300;
+      text-align: left;
+      margin-bottom: 5px;
+      margin-left: 40px;
+    }
+
+    h2 {
+      font-weight: 600;
+      text-align: left;
+      margin-left: 40px;
+    }
   `;
 
   return (
     <Banner>
       <StyledSlider {...settings}>
         <Background>
-          <p>안녕</p>
+          <StudyLabel>study</StudyLabel>
+          <Heading>프론트엔드 스터디 SF5팀</Heading>
+          <Heading2>드림코딩 강의 수강중</Heading2>
+          <Text>Next.js</Text>
         </Background>
-        <div>
-          <img src="https://placekitten.com/1696/565" alt="Banner 2" style={{ width: '100%', height: 'auto' }} />
-        </div>
-        <div>
-          <img src="https://placekitten.com/1696/565" alt="Banner 3" style={{ width: '100%', height: 'auto' }} />
-        </div>
+        <Background>
+          <StudyLabel>study</StudyLabel>
+          <Heading>프론트엔드 스터디 SF5팀</Heading>
+          <Heading2>드림코딩 강의 수강중</Heading2>
+          <Text>Next.js</Text>
+        </Background>
+        <Background>
+          <StudyLabel>study</StudyLabel>
+          <Heading>프론트엔드 스터디 SF5팀</Heading>
+          <Heading2>드림코딩 강의 수강중</Heading2>
+          <Text>Next.js</Text>
+        </Background>
       </StyledSlider>
+      <RoundedRectangle>
+        <h3>스마트하게</h3>
+        <h2>동아리방 예약</h2>
+      </RoundedRectangle>
+      <RoundedRectangle2>
+        <h3>관리자와</h3>
+        <h2>실시간 채팅</h2>
+      </RoundedRectangle2>
     </Banner>
   );
 };
