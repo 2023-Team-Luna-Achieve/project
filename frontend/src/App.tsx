d56da1075db163b8171482f3b613599ddd963a1b
sejongpark <sejong427@sungkyul.ac.kr>
2023-10-19 16:38:18 +0900

    Feat: 회원가입, 로그인 페이지 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import MainPage from './pages/MainPage';
 import Header from './components/layout/Header';
 import NoticePage from './pages/NoticePage';
 import LoginPage from './pages/LoginPage';
+import JoinPage from './pages/JoinPage';
 import CommunityPage from './pages/CommunityPage';
 import AboutPage from './pages/AboutPage';
 import Reservationpage from './pages/ReservationPage';
@@ -19,6 +20,7 @@ const App: React.FC = () => {
           <Route path="Reservation" element={<Reservationpage />} />
           <Route path="Notice" element={<NoticePage />} />
           <Route path="Login" element={<LoginPage />} />
+          <Route path="Join" element={<JoinPage />} />
           <Route path="Community" element={<CommunityPage />} />
         </Routes>
       </div>
--- frontend/src/pages/JoinPage.tsx
+++ frontend/src/pages/JoinPage.tsx
@@ -20,14 +20,22 @@ const Select = styled.select`
   padding: 0.5rem;
 `;
 
+const Option = styled.option``;
+
 const PasswordInput = styled.input`
   width: 100%;
   padding: 0.5rem;
 `;
 
-const LoginPage: React.FC = () => {
+const JoinPage: React.FC = () => {
+  const [affiliation, setAffiliation] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const handleAffiliationChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setAffiliation(event.target.value);
+  };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -37,9 +45,21 @@ const LoginPage: React.FC = () => {
     setPassword(event.target.value);
   };
 
+  const handlePasswordConfirmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirm(event.target.value);
+  };
+
   return (
     <FormContainer>
       <form>
+        <FormGroup>
+          <Label>소속</Label>
+          <Select value={affiliation} onChange={handleAffiliationChange}>
+            <Option value="">선택하세요</Option>
+            <Option value="option1">Techeer</Option>
+            <Option value="option2">Techeer Partners</Option>
+          </Select>
+        </FormGroup>
         <FormGroup>
           <Label>이메일</Label>
           <input type="email" value={email} onChange={handleEmailChange} />
@@ -48,10 +68,14 @@ const LoginPage: React.FC = () => {
           <Label>비밀번호</Label>
           <PasswordInput type="password" value={password} onChange={handlePasswordChange} />
         </FormGroup>
-        <button type="submit">Login</button>
+        <FormGroup>
+          <Label>비밀번호 확인</Label>
+          <PasswordInput type="password" value={passwordConfirm} onChange={handlePasswordConfirmChange} />
+        </FormGroup>
+        <button type="submit">가입하기</button>
       </form>
     </FormContainer>
   );
 };
 
-export default LoginPage;
+export default JoinPage;
