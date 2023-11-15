1ffb7c60d8b2c4eb561372d15c161be85fcdee12
yoojaeyoonGit <jaeyoon321@naver.com>
2023-11-16 02:33:25 +0900

    feat: 이메일 인증 요청 형식 변경

---
+++ backend/src/main/java/backend/backend/auth/dto/EmailRequest.java
@@ -0,0 +1,10 @@
+package backend.backend.auth.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequest {
+    private String email;
+}
