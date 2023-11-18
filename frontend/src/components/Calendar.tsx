8859c3c1176178baa6518634e459034474d000fe
sejongpark <sejong427@sungkyul.ac.kr>
2023-11-18 19:38:23 +0900

    Chore: selectDate 수정 전 세팅

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -2,17 +2,36 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+// 날짜 형식을 조절하여 출력하는 함수
+const formatDate = (date: Date | Date[]): string => {
+  if (date instanceof Date) {
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date
+      .getDate()
+      .toString()
+      .padStart(2, '0')}`;
+  } else {
+    const startDate = date[0];
+    return `${startDate.getFullYear()}-${(startDate.getMonth() + 1).toString().padStart(2, '0')}-${startDate
+      .getDate()
+      .toString()
+      .padStart(2, '0')}`;
+  }
+};
+
 const CalendarComponent: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | Date[]>(new Date());
 
   const handleDateChange = (date: Date | Date[]) => {
     setSelectedDate(date);
+
+    // 선택한 날짜를 콘솔에 출력
+    const formattedDate = formatDate(date);
+    console.log('Selected Date:', formattedDate);
   };
 
   return (
     <div>
       <Calendar
-        // 오류로 인한 타입 형변환을 사용
         onChange={handleDateChange as any}
         value={selectedDate as any}
         formatDay={(_, date) => (date instanceof Date ? date.getDate().toString() : '')}
