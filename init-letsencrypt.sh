b7d3f840431130d095f6573eb7a9429a87296e61
Ubuntu <ubuntu@ip-172-31-6-241.ap-northeast-2.compute.internal>
2023-11-24 08:02:59 +0000

    feat: https 설정 최종

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(achieve-project.store)
 rsa_key_size=4096
 data_path="./devops/nginx/certbot"
 email="jaeyoon321@naver.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
