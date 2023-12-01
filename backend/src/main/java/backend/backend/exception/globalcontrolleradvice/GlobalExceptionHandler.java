83d8385312f18cd7749130dc888cca96e50ef758
yoojaeyoonGit <jaeyoon321@naver.com>
2023-12-01 23:38:16 +0900

    feat: swagger 설정

--- backend/src/main/java/backend/backend/exception/globalcontrolleradvice/GlobalExceptionHandler.java
+++ backend/src/main/java/backend/backend/exception/globalcontrolleradvice/GlobalExceptionHandler.java
@@ -2,15 +2,13 @@ package backend.backend.exception.globalcontrolleradvice;
 
 import backend.backend.exception.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class ControllerAdvice {
+public class GlobalExceptionHandler {
     @ExceptionHandler
     protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
         final ErrorCode errorCode = e.getErrorCode();
