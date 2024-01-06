f3679ea7d777df4064545639f2591f3c772b2987
yoojaeyoonGit <jaeyoon321@naver.com>
2024-01-06 23:35:57 +0900

    feat: 댓글 swagger 문서화

--- backend/src/main/java/backend/backend/noticeboardcomment/controller/CommentController.java
+++ backend/src/main/java/backend/backend/noticeboardcomment/controller/CommentController.java
@@ -52,6 +52,7 @@ import backend.backend.noticeboardcomment.dto.CommentRequestDto;
 import backend.backend.noticeboardcomment.dto.CommentResponse;
 import backend.backend.noticeboardcomment.service.CommentService;
 import backend.backend.user.entity.User;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -59,18 +60,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@ApiOperation(value = "게시글 댓글 API", notes = "회원 작성글의 댓글을 작성한다.")
+@Api(tags = "게시글 댓글 API", description = "회원 작성글의 댓글을 작성한다.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class CommentController {
     private final CommentService commentService;
 
+    @ApiOperation(value = "공지사항 댓글 전체 조회 API", notes = "특정 공지사항의 댓글을 전체조회 한다.")
     @GetMapping("/{noticeBoardId}")
     public List<CommentResponse> getAllCommentsByNoticeBoardId(@PathVariable Long noticeBoardId) {
         return commentService.getAllCommentsByNoticeBoardId(noticeBoardId);
     }
 
+    @ApiOperation(value = "공지사항 생성 API", notes = "특정 공지사항의 댓글을 생성 한다.")
     @PostMapping("/noticeboard/{noticeBoardId}")
     public ResponseEntity<CommentResponse> createComment(@PathVariable("noticeBoardId") Long noticeBoardId,
                                                          @RequestBody CommentRequestDto commentRequestDto,
@@ -79,12 +82,14 @@ public class CommentController {
         return ResponseEntity.ok(createdComment);
     }
 
+    @ApiOperation(value = "공지사항 단일 조회 API", notes = "공지사항 댓글을 단일 조회 한다.")
     @GetMapping("/comments/{commentId}")
         public ResponseEntity<CommentResponse> getCommentById(@PathVariable Long commentId) {
         CommentResponse commentDto = commentService.getCommentById(commentId);
             return ResponseEntity.ok(commentDto);
     }
 
+    @ApiOperation(value = "공지사항 삭제 API", notes = "공지사항 댓글을 삭제 한다.")
     @DeleteMapping("/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
