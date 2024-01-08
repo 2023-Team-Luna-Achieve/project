3a1ef7f723c3748ee79d17503ef1fd8e21633fe7
yoojaeyoonGit <jaeyoon321@naver.com>
2024-01-09 01:56:43 +0900

    fix: 그린 블루 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ cd $REPOSITORY
 DOCKER_APP_NAME=achieve
 
 # 실행중인 blue가 있는지 확인
-# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
 #EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose-blue.yml ps)
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose-blue.yml ps | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; print $0}' | sed 's/^[ \t]*//')
 # 테스트 배포 시작한 날짜와 시간을 기록
